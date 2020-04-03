76e84bc31cc5e9c8cd16ffacb6bd6c5680eff519
Jurgen <jurgen.stevens@yahoo.com>
2020-04-03 01:50:55 -0500

    Added a new file with a return message.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var c1 = new MyClass();
+            Console.WriteLine($"Hello World! {c1.ReturnMessage()}");
         }
     }
 }
